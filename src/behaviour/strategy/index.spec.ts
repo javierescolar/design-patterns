15ecd7745242ec8e45c64e077e0155c96f20dba9
Javi <jescolar@madisa.es>
2022-04-13 08:49:10 +0200

    Updates Common

---
+++ src/behaviour/strategy/index.spec.ts
@@ -0,0 +1,21 @@
+
+import { Checkout } from "./src/checkout";
+import { PayPalStrategy } from "./src/paypal.strategy";
+
+const checkout = new Checkout();
+
+let userId = "usr-001",
+    useEmail = "eduardo@kodoti.com",
+    amount = 2000;
+
+describe("Strategy Pattern", () => {
+    it("successful paypal usage", () => {
+        checkout.setStrategy(
+            new PayPalStrategy(useEmail)
+        );
+        
+        const { isSuccess } = checkout.execute(userId, amount);
+
+        expect(isSuccess).toBeTruthy()
+    });
+});
